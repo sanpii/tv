a83f59f6a3bc687573ad8079cc1714c0d09a1c5f
Sanpi <sanpi@homecomputing.fr>
2022-11-08 14:47:59 +0100

    Creates axum app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app = axum::Router::new();
+
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
 }
